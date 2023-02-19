159fcdf7ac2e5e3372a0d0d25a2b955f8238d8f1
Vlad Shamov <holovlad@yandex.ru>
2023-02-19 21:05:22 +1000

    Added mvp of Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
+    colors: {
+      basic: "#041C32",
+      "darkest-red": "#F2F7A1",
+      "dark-red": "#950101",
+      "basic-red": "#6D67E4",
+    },
+    fontFamily: {
+      tourney: ["Tourney", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
